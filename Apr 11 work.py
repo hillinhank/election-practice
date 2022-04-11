725d4aeeed2f9bf9545c6bb30395205f98423263
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-04-11 12:10:25 -0500

    still not working

--- Apr 11 work.py
+++ Apr 11 work.py
@@ -4,7 +4,7 @@ import csv
 
 import os
 
-
+total_votes = 0
 
 file_to_load = '/Users/user/Downloads/election_results-2.csv'
 file_to_save = '/Users/user/election_analysis.txt'
@@ -15,7 +15,10 @@ election_data = open(file_to_load, 'r')
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
-    print(headers)
+    for row in file_reader:
+        total_votes += 1
+print(total_votes)
+        
     
 
 
